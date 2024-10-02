7dea5a8f6b157ef06018f9020bcd626c020544d0
fsh-23 <fsh23f12xd@fukui-ed.jp>
2024-10-02 14:48:26 +0900

    ??

---
+++ main.js
@@ -0,0 +1,8 @@
+/** @type {HTMLBodyElement} */
+'use strict';
+
+window.addEventListener('DOMContentLoaded',() => {
+    const element = document.getElementsByClassName('flex');
+    console.log(element);
+    
+})
